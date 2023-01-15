d35504aaea02e370161a6862a523e340dc74f65a
Stenford <76819313+DenisGradov@users.noreply.github.com>
2023-01-15 20:27:58 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
-token='5918451332:AAEDztTWfsZfp1ulURcQlVBkeV6QALTCmhk'
-admin=5640543343
-channel=-1001739153721
-channelLink='t.me/MultihumanNews'
-lolz='https://zelenka.guru/threads/4726315/'
+token='токен бота'
+admin=ид админа
+channel=-ид канала
+channelLink='линк'
+lolz='тема'
 howFindCurs='https://telegra.ph/Kak-najti-kurs-01-06'
-autor='t.me/multihuman'
-logs=-1001780244825
+autor='тг'
+logs=-ид канала
